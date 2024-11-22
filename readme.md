a9287ba4dd5eca4ff5793979b64700b87eb5002f
ricardomarques28 <contato.ricardomarques@gmail.com>
2024-11-21 22:33:15 -0300

    Projeto final Detona Ralph

---
+++ readme.md
@@ -0,0 +1,58 @@
+
+# 💡 Detona Ralph - Jogo Interativo 🎮💥
+
+Bem-vindo ao repositório Detona Ralph, um projeto inspirado no clássico arcade, onde você testa seus reflexos e precisão em um ambiente cheio de ação! 🚀
+
+## 🕹️ Sobre o Projeto - Objeivo:
+O Detona Ralph é um jogo dinâmico no qual o objetivo é acertar os alvos certos (janelas com o Ralph) e evitar os erros (janelas erradas). Ele conta com uma interface retrô, sons imersivos e mecânicas simples, mas desafiadoras!
+
+✨ Funcionalidades
+- Timer Dinâmico: Corrida contra o tempo com feedback visual.
+- Sistema de Vidas: Perdeu? Você pode tentar novamente, mas cuidado com suas 3 vidas!
+- Som Personalizado: Efeitos sonoros para acertos, erros e música de fundo.
+- Design Responsivo: Perfeito para diferentes dispositivos.
+- Interface Retrô: Elementos visuais inspirados no mundo do Ralph.
+
+## ⚙️ Tecnologia aplicadas:
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+![Git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white)
+
+## 🎨 Estrutura do Projeto
+bash
+Copiar código
+detona-ralph/  
+├── src/  
+│   ├── audios/          # Arquivos de áudio (.m4a)  
+│   ├── css/             # Arquivos de estilo  
+│   │   ├── main.css  
+│   │   ├── reset.css  
+│   ├── images/          # Imagens para o design do jogo  
+│   │   ├── player.png  
+│   │   ├── wall.png  
+│   │   ├── window.png  
+│   ├── scripts/         # Arquivos JavaScript  
+│       ├── engine.js  
+├── index.html           # Arquivo principal  
+└── README.md            # Documentação do projeto  
+
+## 🎯 Como Jogar
+
+- Clique em Start para iniciar o jogo.
+- Acione o botão esquerdo do mouse sobre as janelas onde Ralph aparece.
+- Evite clicar em janelas erradas para não perder pontos ou vidas!
+- Vença acumulando pontos antes que o tempo acabe.
+
+### Controles:
+- Botão Start: Começa o jogo.
+- Botão Reset: Reinicia a partida.
+
+
+
+Divirta-se quebrando tudo! 💪🎯
+
---
+++ src/scripts/engine.js
@@ -0,0 +1,131 @@
+const state = {
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+    startButton: document.querySelector("#start-btn"),
+    resetButton: document.querySelector("#reset-btn"),
+    life: document.querySelector("#life"),
+    backgroundMusic: document.getElementById("#background-music")
+  },
+  values: {
+    timerId: null,
+    countDownTimerId: null,
+    gameVelocity: 600,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60,
+    countLife: 3,
+  },
+};
+
+function playSound(audioname) {
+  let audio = new Audio(`./src/audios/${audioname}.m4a`);
+  audio.volume = 0.2;  
+  audio.play();
+}
+
+function countDown() {
+  state.values.countDownTimerId = setInterval(() => {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+      clearInterval(state.values.countDownTimerId);
+      clearInterval(state.values.timerId);
+
+      if (state.values.result >= 50) {
+        alert("🏆 You Win! O seu resultado foi: " + state.values.result);
+        location.reload();
+      } else {
+        alert(
+          "⚠️ Ops! O seu resultado foi: " +
+            state.values.result +
+            " Tente outra vez"
+        );
+
+        state.values.countLife--;
+        state.view.life.textContent = state.values.countLife;
+      }
+
+      if (state.values.countLife === 0) {
+        alert("👎 Game Over! Você perdeu todas as vidas! O jogo será reiniciado.");
+        setTimeout(() => {
+          location.reload();
+        }, 2000);
+      } else {
+        state.view.squares.forEach((square) => {
+          square.classList.remove("enemy");
+          square.classList.remove("enemyError");
+        });
+        resetGame();
+      }
+    }
+  }, 1000);
+}
+
+function moveEnemy() {
+  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
+function randomSquare() {
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy");
+    square.classList.remove("enemyError");
+  });
+
+  let randomNumber = Math.floor(Math.random() * 9);
+  let randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox() {
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+        playSound("hit");
+        square.classList.add("sureEnemy");
+        setTimeout(() => {
+          square.classList.remove("sureEnemy");
+        }, 500);
+        
+      } else {
+        playSound("error");
+        square.classList.add("enemyError");
+        
+        setTimeout(() => {
+          square.classList.remove("enemyError");
+        }, 500);
+      }
+    });
+  });
+}
+
+function resetGame() {
+  state.values.currentTime = 10;
+  state.view.timeLeft.textContent = state.values.result;
+  state.values.result = 0;
+  state.view.timeLeft.textContent = 0;
+  state.view.score.textContent = state.values.result;
+  state.view.startButton.hidden = false;
+}
+
+function initialize() {
+  state.view.startButton.addEventListener("click", () => {
+    moveEnemy();
+    addListenerHitBox();
+    countDown();
+    state.view.startButton.hidden = true;
+  });
+
+  state.view.resetButton.addEventListener("click", () => {
+    location.reload();
+  });
+}
+
+initialize();
